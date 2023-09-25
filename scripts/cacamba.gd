ce5e709386616fb3bde0eb2c83e1394b1f408382
Alexandre Massaro <alexandremassaro@proton.me>
2023-09-25 17:42:45 -0300

    Updated documentation comments for the player_character and cacamba scenes

--- scripts/cacamba.gd
+++ scripts/cacamba.gd
@@ -1,15 +1,11 @@
 extends StaticBody2D
+## Contém todos os métodos e propriedades do objeto caçamba.
+##
+## Controla as animações e ações do objeto caçamba.
+##
 
-
+## Uma referência ao node AnimationPlayer.
 @onready var animation_player = $AnimationPlayer
-
-
-func abrir_tampa():
-	animation_player.play("open")
-
-
-func fechar_tampa():
-	animation_player.play("close")
 
 
 func _on_open_close_area_body_entered(body):
@@ -20,3 +16,13 @@ func _on_open_close_area_body_entered(body):
 func _on_open_close_area_body_exited(body):
 	if body.is_in_group("player"):
 		fechar_tampa()
+
+
+## Toca a animação de abrir a tampa da caçamba.
+func abrir_tampa():
+	animation_player.play("open")
+
+
+## Toca a animação de fechar a tampa da caçamba.
+func fechar_tampa():
+	animation_player.play("close")
--- scripts/player_chararcter.gd
+++ scripts/player_chararcter.gd
@@ -1,13 +1,30 @@
 extends CharacterBody2D
+## Contém todos os métodos e propriedades do personagem principal do jogador.
+##
+## Controla o movimento, velocidade, animações e ações do personagem.
+##
 
+## A velocidade que personagem se move pela tela.
 @export var move_speed : float = 50.0
 
-var carying : Trash = null
-var items_in_range = []
-
+## A posição que define qual animação será disparada quando a cena for 
+## carregada pela primeira vez.
 @export var starting_direction : Vector2 = Vector2(0, 1)
 
+## O objeto que o personagem principal está segurando.
+## Deve ser do tipo Trash, ou nulo.
+var carying : Trash = null
+
+## Uma lista de todos os items que o personagem pricipal pode agarrar. 
+## FIFO. O personagem vai pegar o primeiro item dessa lista.
+var items_in_range = []
+
+## Uma referência ao node AnimationTree.
+## Esse node controla as animações do personagem.
 @onready var animation_tree = $AnimationTree
+
+## Uma referência ao state machine que define a animação 
+## que está sendo tocada pelo AnimationTree.
 @onready var state_machine = animation_tree["parameters/playback"]
 
 
@@ -17,8 +34,10 @@ func _ready():
 
 func _input(event):
 	if event.is_action_released("pick_up_item"):
-		drop_item()
-		pick_up_item()
+		if carying:
+			drop_item()
+		else:
+			pick_up_item()
 
 
 func _physics_process(_delta):
@@ -32,12 +51,9 @@ func _physics_process(_delta):
 	if input_direction != Vector2.ZERO:
 		update_animation_parameters(input_direction)
 		change_pick_up_position(input_direction)
-	
-	# Update velocity
-	velocity = input_direction * move_speed
+		velocity = input_direction * move_speed
 	
 	pick_new_state()
-		
 	move_and_slide()
 	
 	if carying:
@@ -55,36 +71,40 @@ func _on_pick_up_area_area_exited(area):
 	
 	if item_index != -1:
 		items_in_range.remove_at(item_index)
-	
-	# print(items_in_range.size())
 
 
+## Pega o primeiro item da lista items_in_range.[br]
+## [color=yellow]Aviso:[/color] Sempre se certifique que a propriedade 'carying'
+## é null antes de chamar esse método, senão ele nunca conseguirá soltar o item.
 func pick_up_item():
-	for item in items_in_range:
-		carying = item
-		break
-	
-	if carying:
-		items_in_range.remove_at(items_in_range.find(carying))
+	if len(items_in_range) > 0:
+		carying = items_in_range[0]
 
 
+## Solta o item que está sendo carregado.
 func drop_item():
 	carying = null
 
-
+## Atualiza o node AnimationTree para alternar as animações de acordo com a 
+## direção do movimento e o input do jogador (direita, esquerda, cima e baixo).
+## Altera a direção que o personagem está olhando.
 func update_animation_parameters(move_input : Vector2):
 	# Don't update if there is no user input
-	if not move_input == Vector2.ZERO: 
+	if move_input != Vector2.ZERO: 
 		animation_tree.set("parameters/walk/blend_position", move_input)
 		animation_tree.set("parameters/idle/blend_position", move_input)
 
 
+## Atualiza o state machine que controla as animações para que alterne 
+## entre as animações idle (parado) ou walk (andando).
 func pick_new_state():
-	if not velocity == Vector2.ZERO:
+	if velocity != Vector2.ZERO:
 		state_machine.travel("walk")
 	else:
 		state_machine.travel("idle")
 
 
+## Atualiza a posição do item sendo carregado para que fique sempre 
+## a frente do personagem principal.
 func change_pick_up_position(pos : Vector2):
 	$PickUpPosition.position = pos * 10
